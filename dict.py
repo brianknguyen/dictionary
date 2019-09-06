85d7958f89e8c4a310440cd7787465f4f3c9acb6
Brian Nguyen <bktnguyen@ucdavis.edu>
2019-09-05 20:11:16 -0700

    handle acronyms

--- dict.py
+++ dict.py
@@ -11,7 +11,10 @@ def translate(w):
     
     elif w.title() in data:  
         return data[w.title()]
-        
+
+    elif w.upper() in data: 
+        return data[w.upper()]
+
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
